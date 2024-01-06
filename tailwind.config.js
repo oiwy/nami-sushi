dcf6f88356d22992e78f99919dd4c06c39c503fc
Aleksey Popov <alekseypopov.dev@gmail.com>
2024-01-07 02:08:23 +0700

    feat: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,28 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "10px",
+        sm: "10px",
+        lg: "20px",
+      },
+    },
+    colors: {
+      primary: "#312525",
+      secondary: "#B3A296",
+      content: "#808080",
+      header: "#062D4E",
+      footer: "#243139",
+      white: "#FFFFFF",
+      black: "#212121",
+      card: "#FAFAFA",
+    },
+    fontFamily: {
+      sans: ["Open Sans", "sans-serif"],
+      neucha: ["Neucha", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
